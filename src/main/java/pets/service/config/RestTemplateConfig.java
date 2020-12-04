4a636eb8b95236c6c6f314511703c25fcb83f147
Bibek Aryal <34973169+bibekaryal86@users.noreply.github.com>
2020-12-04 10:12:04 -0700

    Update Rest Template Config to Increase Timeouts (#5)

--- src/main/java/pets/service/config/RestTemplateConfig.java
+++ src/main/java/pets/service/config/RestTemplateConfig.java
@@ -23,8 +23,8 @@ public class RestTemplateConfig {
 
     private ClientHttpRequestFactory clientHttpRequestFactory() {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setReadTimeout(5000);
-        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(15000);
+        factory.setConnectTimeout(15000);
         return factory;
     }
 }
